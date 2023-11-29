cddb0cc10bc97a3e41db9262564da4d39a117b6a
Ausha123 <aushasandeepa569@gmail.com>
2023-11-29 13:44:19 +0530

    angular query params & path params done

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
+
+
+export class AboutComponent implements OnInit{
+
+  constructor(private activatedRoute:ActivatedRoute) {
+  }
+
+
+    param: any;
+    queryParam:any;
+
+  ngOnInit(): void {
+    console.log(this.activatedRoute);
+    this.param = this.activatedRoute.snapshot.params['username'];
+    this.queryParam=this.activatedRoute.snapshot.queryParams['course'];
+  }
 
 }
